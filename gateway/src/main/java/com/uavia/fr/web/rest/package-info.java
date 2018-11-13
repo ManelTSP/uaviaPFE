b28af42876ca5b986cbf407f7fcf4474aa537d53
Khaled MOALLA <khaled.moalla@telecom-sudparis.eu>
2018-11-13 01:55:50 +0100

    cleaned junk files

--- gateway/src/main/java/com/uavia/fr/web/rest/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Spring MVC REST controllers.
- */
-package com.uavia.fr.web.rest;
--- microservice1/src/main/java/com/uavia/fr/web/rest/vm/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * View Models used by Spring MVC REST controllers.
- */
-package com.uavia.fr.web.rest.vm;
